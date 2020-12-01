b338015577e82e656d0ce1f5c99caa9b465baf48
Gareth Maddock <gtmaddock@gmail.com>
2020-12-01 21:40:54 +0000

    day 1, part 2: initialise

--- puzzles/day_01.R
+++ puzzles/day_01.R
@@ -40,3 +40,18 @@ nums <-
   )
 
 day_01_part1(nums)
+
+#' The Elves in accounting are thankful for your help; one of them even offers 
+#' you a starfish coin they had left over from a past vacation. They offer you a
+#' second one if you can find three numbers in your expense report that meet the
+#' same criteria.
+#' 
+#' Using the above example again, the three entries that sum to 2020 are 979, 
+#' 366, and 675. Multiplying them together produces the answer, 241861950.
+#' 
+#' In your expense report, what is the product of the three entries that sum to 
+#' 2020?
+
+day_01_part2 <- function(nums) {
+  
+}
